b03ac3cb42d26dbef5aaac467757ce0229cac30b
Sean2747 <shanjiangca@gmail.com>
2026-02-13 15:27:46 -0800

    latest working version

--- src/scout_bringup/scout_bringup/nav_pose_publisher.py
+++ src/scout_bringup/scout_bringup/nav_pose_publisher.py
@@ -13,15 +13,15 @@ def main() -> None:
     initial_pose = PoseStamped()
     initial_pose.header.frame_id = 'map'
     initial_pose.header.stamp = navigator.get_clock().now().to_msg()
-    initial_pose.pose.position.x = 0.479
-    initial_pose.pose.position.y = 0.-994
+    initial_pose.pose.position.x = 0.562
+    initial_pose.pose.position.y = -0.802
     initial_pose.pose.orientation.z = 0.0
     initial_pose.pose.orientation.w = 1.0
     navigator.setInitialPose(initial_pose)
 
     navigator.waitUntilNav2Active()
 
-    yaw_deg = 180
+    yaw_deg = 90
     quaternion = quaternion_from_euler(0, 0, yaw_deg)
 
     goal_pose = PoseStamped()
