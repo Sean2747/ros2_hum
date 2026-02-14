82ec3631cabe143dc583955e31651f49c17ac187
Sean2747 <shanjiangca@gmail.com>
2026-02-13 17:20:10 -0800

    pose_tracker debugged

--- src/scout_bringup/scout_bringup/pose_tracker.py
+++ src/scout_bringup/scout_bringup/pose_tracker.py
@@ -1,19 +1,39 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import QoSProfile, QoSDurabilityPolicy
 from tf_transformations import euler_from_quaternion
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from nav_msgs.msg import OccupancyGrid
 
 class PoseTracker(Node):
     def __init__(self):
         super().__init__('pose_tracker')
-        self.subscriber = self.create_subscription(PoseWithCovarianceStamped, '/amcl_pose', self.output_data, 10)
+        qos = QoSProfile(depth=10)
+        qos.durability = QoSDurabilityPolicy.TRANSIENT_LOCAL
+        self.amcl_subscriber = self.create_subscription(PoseWithCovarianceStamped, '/amcl_pose', self.output_amcl_data, qos)
+        #self.map_subscriber = self.create_subscription(OccupancyGrid, '/map', self.output_map_data, qos)
 
-    def output_data(self, data):
-        current_x = data.pose.position.x
-        current_y = data.pose.position.y
-        yaw_deg = euler_from_quaternion(data.pose.orientation)
+    def output_amcl_data(self, data):
+        self.get_logger().info("Hello amcl")
+        current_x = data.pose.pose.position.x
+        current_y = data.pose.pose.position.y
+
+        quaternion_values = [
+            data.pose.pose.orientation.x,
+            data.pose.pose.orientation.y,
+            data.pose.pose.orientation.z,
+            data.pose.pose.orientation.w
+        ]
+
+        yaw_deg = euler_from_quaternion(quaternion_values)
 
         self.get_logger().info(f"({current_x}, {current_y}) | angle: {yaw_deg}")
+        #self.get_logger().info(data)
+
+    def output_map_data(self, data):
+        print("print map")
+        self.get_logger().info("Hello map")
+        self.get_logger().info(data)
 
 
 def main(args=None):
