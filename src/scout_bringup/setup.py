b741944f54b509245d6def660403265c3ce6321e
Howard-GitHub <howard.l.general@gmail.com>
2026-02-27 16:55:40 -0800

    Add grid_tracker node in setup.py

--- src/scout_bringup/setup.py
+++ src/scout_bringup/setup.py
@@ -28,7 +28,8 @@ setup(
         'console_scripts': [
             "nav_pose_publisher = scout_bringup.nav_pose_publisher:main",
             "nav_waypoint_publisher = scout_bringup.nav_waypoint_publisher:main",
-            "pose_tracker = scout_bringup.pose_tracker:main"
+            "pose_tracker = scout_bringup.pose_tracker:main",
+            "grid_tracker = scout_bringup.grid_tracker:main"
         ],
     },
 )
